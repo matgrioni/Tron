01a1a4ad43f2bd223ae21a3db472b7309847f8ea
Matias Grioni <matgrioni@gmail.com>
2015-06-03 23:47:22 -0400

    Updated PygameHelper.py

--- src/PygameHelper.py
+++ src/PygameHelper.py
@@ -11,7 +11,14 @@
 # Extend this class and override the different handler,
 # update and draw methods.
 #
-# This class is inspired from phyces original code.
+# This class is inspired from phyces original code for
+# PygameHelper. The one major difference is that the
+# original used the function handleEvents with if blocks
+# to check for all the current events. This implementation
+# defines callbacks and the events for which these callbacks
+# are used.
+# This gives the user more ability to control what and when
+# is being called for the events.
 ###########################################################
 
 import pygame
@@ -35,10 +42,54 @@ def PygameHelper:
         # defined callback. Each event has one possible callback
         # and if a callback is assigned to an event code which is
         # already assigned then the new will overwrite the old.
-        self.keyCallbacks = {};
-        self.mouseCallbacks = {};
+        self.eventCallbacks = {};
 
-    # Adds a 
-    def addKeyCallback(self, code, callback) {
-        self.keyCallbacks[code] = callback
+    # Adds a callback for the keyboard event code. All codes have one
+    # unique callback. event should be a tuple where the first entry
+    # is the key code, and the second entry is the event type.
+    def addEventCallback(self, event, callback) {
+        self.eventCallbacks[event] = callback
     }
+
+    # Removes the callback for the provided event and returns the
+    # previously added callback or None if there was none defined
+    # for the event.
+    def removeEventCallback(self, event) {
+        return self.eventCallbacks(event, None);
+    }
+
+    # The following functions _handleEvents, update, and draw, are
+    # called once every loop iteration as seen in mainLoop.
+
+    # For each current event in the queue check it against the codes
+    # for the defined callbacks. This function does not have to
+    # defined by the client, nor should it be called publicly.
+    #
+    # To control event handlers use the callback access methods.
+    def _handleEvents(self):
+        pass
+
+    # Update the game state after one loop iteration. This helper class
+    # does not implement this but a class that does implements the game
+    # logic here.
+    def update(self):
+        pass
+    
+    # Draw whatever is needed to the screen. This function is defined
+    # in classes that derive from PygameHelper.
+    def draw(self):
+        pass
+
+    # Runs the current game definition in this instance of PygameHelper.
+    def execute(self, fps=0):
+        self.running = True
+        self.fps = fps
+
+        # Accept input and update the screen, 
+        while self.running:
+            self._handlEvents();
+            self.update();
+            self.draw();
+            pygame.display.flip()
+
+            self.clock.tick(self.fps)
