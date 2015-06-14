22f3a3a7254fa56373bb56d678f2ec1781062f75
Matias Grioni <matgrioni@gmail.com>
2015-06-13 21:58:21 -0400

    Added controls for player 2 and collision detection

--- src/main.py
+++ src/main.py
@@ -17,54 +17,54 @@ class Game(PygameHelper):
     def __init__(self, size=(640, 480)):
         super(Game, self).__init__(size)
 
-        self.turning = False
+        self.p1 = LineRider(10, size[1] / 2, Direction.RIGHT)
+        self.p2 = LineRider(size[0] - 15, size[1] / 2, Direction.LEFT)
 
-        self.p1 = LineRider(10, 240, Direction.RIGHT)
-        self.p2 = LineRider(625, 240, Direction.LEFT)
+        self.alive = {self.p1: 1, self.p2: 1}
 
-        # Add the necessary callbacks for the movement
-        # of the players.
-        self.addEventCallback((KEYDOWN, (K_RIGHT, K_LEFT)), self._dirKeydown)
-        self.addEventCallback((KEYUP, (K_RIGHT, K_LEFT)), self._dirKeyup)
-        self.addEventCallback((KEYDOWN, K_RIGHT), self._p1TurnRight)
-        self.addEventCallback((KEYDOWN, K_LEFT), self._p1TurnLeft)
+        # Add callbacks for moving the players.
+        self.addEventCallback((KEYDOWN, (K_RIGHT, K_LEFT)), self._p1DirKeydown)
+        self.addEventCallback((KEYUP, (K_RIGHT, K_LEFT)), self._p1DirKeyup)
+
+        self.addEventCallback((KEYDOWN, (K_a, K_d)), self._p2DirKeydown)
+        self.addEventCallback((KEYUP, (K_a, K_d)), self._p2DirKeyup)
 
     # Simply move each player along as needed
     def update(self):
         self.p1.update()
         self.p2.update()
 
+        self.alive[self.p1] = not self.p1.collides(self.p2)
+        self.alive[self.p2] = not self.p2.collides(self.p1)
+
     def draw(self):
         self.p1.draw(self.screen)
         self.p2.draw(self.screen)
 
-    def _p1TurnRight(self, event):
-        if not self.turning:
-            self.p1.turnRight()
+        if self.alive[self.p1] == 0 or self.alive[self.p2] == 0:
+            self.quit()
 
-    def _p1TurnLeft(self, event):
-        if not self.turning:
+    def _p1DirKeydown(self, event):
+        if event.key == K_RIGHT:
+            self.p1.turnRight()
+        elif event.key == K_LEFT:
             self.p1.turnLeft()
 
-    def _p2TurnRight(self, event):
-        if not self.turning:
+        self.p1.turnable = False
+
+    def _p1DirKeyup(self, event):
+        self.p1.turnable = True
+
+    def _p2DirKeydown(self, event):
+        if event.key == K_d:
             self.p2.turnRight()
+        elif event.key == K_a:
+            self.p2.turnLeft()
 
-    def _p2TurnLeft(self, event):
-        if not self.turning:
-            self.p2.turnRight()
-
-    # These methods are called when a key to turn the line
-    # riders are moved, so that we can keep track of each
-    # event as it happens. This way the riders don't turn
-    # more than once per keypress.
-    def _dirKeydown(self, event):
-        self.turning = True
-
-    def _dirKeyup(self, event):
-        self.turning = False
+    def _p2DirKeyup(self, event):
+        self.p2.turnable = True
 
 # If this is the main program, run the game at 60 fps
 if __name__ == "__main__":
     g = Game()
-    g.execute(40)
+    g.execute(45)
