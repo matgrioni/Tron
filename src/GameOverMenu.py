ba17e2970f752a96905adb07bb054adb076ef5a9
Matias Grioni <matgrioni@gmail.com>
2015-06-21 13:19:54 -0400

    New menus for the current game contexts

---
+++ src/GameOverMenu.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+###########################################################
+# Author: Matias Grioni
+# Created: 6/21/15
+#
+# The menu that pops up once at least one of the players
+# dies. Similar to the pause menu however no resume option.
+###########################################################
+
+from Menu import Menu
+from PygameHelper import TextDisp
+
+class GameOverMenu(Menu):
+    def __init__(self, scores, parent=None, size=(640, 480),
+                 fill=(255, 255, 255)):
+        options = ["Next game", "Main menu", "Quit"]
+        super(GameOverMenu, self).__init__(options, parent, size, fill)
+
+        self.addOptionCallback("Next game", self._startGameOver)
+        self.addOptionCallback("Main menu", self.back, count=2)
+        self.addOptionCallback("Quit", self.quit)
+
+        self.p1Score = TextDisp(10, 10, "Player 1: " + str(scores[0]),
+                                fontsize=15)
+        self.p2Score = TextDisp(self.size[0] - 150, 10,
+                                "Player 2: " + str(scores[1]),
+                                fontsize=15)
+
+    def draw(self):
+        super(GameOverMenu, self).draw()
+        self.p1Score.draw(self.screen)
+        self.p2Score.draw(self.screen)
+
+    def _startGameOver(self):
+        self.parent.reset()
+        self.back()
---
+++ src/PauseMenu.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+###########################################################
+# Author: Matias Grioni
+# Created: 6/18/15
+#
+# Defines the pause menu for the Game using the Menu
+# library.
+###########################################################
+
+from Menu import Menu
+from PygameHelper import TextDisp
+
+class PauseMenu(Menu):
+    def __init__(self, scores, parent=None, size=(640, 480), fill=(255, 255, 255)):
+        options = ["Resume", "Start over", "Main menu", "Settings", "Quit"]
+        super(PauseMenu, self).__init__(options, parent, size, fill)
+
+        self.addOptionCallback("Resume", self._resumeGame)
+        self.addOptionCallback("Start over", self._startGameOver)
+        self.addOptionCallback("Main menu", self.back, count=2)
+        self.addOptionCallback("Quit", self.quit)
+
+        self.p1Score = TextDisp(10, 10, "Player 1: " + str(scores[0]), fontsize=15)
+        self.p2Score = TextDisp(self.size[0] - 150, 10,
+                                "Player 2: " + str(scores[1]), fontsize=15)
+
+    def draw(self):
+        super(PauseMenu, self).draw()
+
+        self.p1Score.draw(self.screen)
+        self.p2Score.draw(self.screen)
+
+    def _resumeGame(self):
+        self.parent.resetTimer()
+        self.back()
+
+    def _startGameOver(self):
+        self.parent.reset()
+        self.back()
